36065027bdae5b68cd171e87d446b24a736a0bbf
Klaas Eenkhoorn <klaas@odroid.int.kdedesign.nl>
2019-01-20 22:41:48 +0100

    Altered buildscripts

--- build-7.6.42.sh
+++ build-7.6.42.sh
@@ -5,5 +5,4 @@ MYSQLMNR=42
 CPU=4
 
 docker build --network host --build-arg CPU=${CPU} --build-arg MYSQLMJR=${MYSQLMJR} --build-arg MYSQLMNR=${MYSQLMNR} -t kdedesign/mysql-server:${MYSQLMJR}.${MYSQLMNR} .
-docker tag kdedesign/mysql-server:${MYSQLMJR}.${MYSQLMNR} kdedesign/mysql-server:${MYSQLMJR}.${MYSQLMNR}
 docker tag kdedesign/mysql-server:${MYSQLMJR}.${MYSQLMNR} kdedesign/mysql-server:${MYSQLMJR}
