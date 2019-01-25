53cdb6d39621e0ed0f91420199aa2fa09723aa29
Klaas Eenkhoorn <klaas@odroid.int.kdedesign.nl>
2019-01-25 09:17:01 +0100

    Creation of mysql 7.6.42 image does not create mysql:latest tag.

--- build-7.6.42.sh
+++ build-7.6.42.sh
@@ -13,4 +13,4 @@ cd ./build-context
 # Start build
 docker build --network host --build-arg CPU=${CPU} --build-arg MYSQLMJR=${MYSQLMJR} --build-arg MYSQLMNR=${MYSQLMNR} -t kdedesign/mysql-server:${MYSQLMJR}.${MYSQLMNR} .
 docker tag kdedesign/mysql-server:${MYSQLMJR}.${MYSQLMNR} kdedesign/mysql-server:${MYSQLMJR}
-docker tag kdedesign/mysql-server:${MYSQLMJR}.${MYSQLMNR} kdedesign/mysql-server:latest
+#docker tag kdedesign/mysql-server:${MYSQLMJR}.${MYSQLMNR} kdedesign/mysql-server:latest
