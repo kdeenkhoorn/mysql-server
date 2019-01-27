12c6d03899fefb9b2fa68e80c94368c4db7d51c1
Klaas Eenkhoorn <klaas@odroid.int.kdedesign.nl>
2019-01-27 21:39:16 +0100

    MySQL 5 is not buildable with this image, this needs more TLC so this script is removed.

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d --restart always -p 3306:3306 -v /data/docker/mysqld:/var/lib/mysql --name=mysqld kdedesign/mysql-server:8.0.14
+docker run -d --restart always -p 3306:3306 -v /data/docker/mysqld:/var/lib/mysql --name=mysqld kdedesign/mysql-server:8.0
